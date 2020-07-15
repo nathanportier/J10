db3fdadbf1dc06afa160d0350bff2728cb2e658e
nathanportier <nportiern@gmail.com>
2020-07-15 21:17:33 +0200

    Create requète-select.sql

---
+++ EXO 01/requète-select.sql
@@ -0,0 +1,18 @@
+
+127.0.0.1/basetest01/produit/		http://localhost/phpmyadmin/sql.php?server=1&db=basetest01&table=produit&pos=0
+ Affichage des lignes 0 -  6 (total de 7, traitement en 0,0006 seconde(s).)
+
+SELECT * FROM `produit`
+
+
+ID	Nom	Desc	Prix	Quantité	
+1	T-shirt rouge	T-shirt coton de couleur rouge	"16"5	
+2	T-shirt vert	T-shirt coton de couleur vert 	"16"6	
+3	T-shirt argent 	T-shirt coton de couleur argent "16"8	
+4	short bleu	short jean de couleur bleu 	"17"5	
+5	short vert	short jean de couleur verte	"20"5	
+6	gilet noir	gilet de couleur noir "20"10	
+7	veste argent 	veste de couleur argentée	"35"3	
+
+
+SELECT * Nom
